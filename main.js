8f90ac0373a3670b6775b9f1417dd5fe936b0081
Romana-Afroz <18203042@iubat.edu>
2021-09-02 14:45:46 +0600

    updated

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
 const searchBook= () => {
-    const inputField=document.getElementById('search-field');
-    const searchText=inputField.value;
-    console.log(searchText);
+    const searchField=document.getElementById('search-field');
+    const searchText=searchField.value;
+    //console.log(searchText);
+    searchField.value= '';
     const url=` https://openlibrary.org/search.json?q=${searchText}`;
-    // fetch=(url)
-    // .then(res => res.json());
-    //  .then(data => console.log(data.docs));
     fetch(url)
-			.then((res) => res.json())
-			.then((data) => displayResult(data.results));
-    inputField.value= ' ';
+			.then(res => res.json())
+			.then(data => displaySearchResult(data.docs));
+    //console.log(url);
 }
-searchBook();
-  
-const displayResult=document.getElementById('display-result');
- const displayBook=books =>{
-    let count=0;
+//searchBook();
+ const displaySearchResult=books => {
+    const displayResult=document.getElementById('display-result');
+    books.forEach(book => {
+        //console.log(book);
+        const div=document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML=`     
+        <div class="card mx-auto" style="width: 18rem; height:40rem;">
+        <img  src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid" alt="books-image">  
+        <div class="card-body">
+            <h5 class="card-title">${book.title}</h5>
+            <p class="card-text"> <span class="fw-bold">Author:</span> ${book.author_name}</p>
+            <p class="card-text"> <span class="fw-bold">Publisher:</span> ${book.publisher}</p>
+            <a href="#" class="btn btn-primary">Read/Borrow</a> 
+        </div>
+        <div class="card-footer">
+             <small class="text-muted">First Published: ${book.first_publish_year}</small>
+        </div>
+    </div>
+    `;
+
+      displayResult.appendChild(div);
+    })
  }
